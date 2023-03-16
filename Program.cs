90a1780dd9104f9615d014e5d2fd261ffea9c332
dimelouder <127084032+dimelouder@users.noreply.github.com>
2023-03-16 12:29:14 +0200

    2

--- Program.cs
+++ Program.cs
@@ -36,15 +36,34 @@ namespace ConsoleApp1
             int[] array = {8, -1, 4, 16, -5, 3};
             int max = array[0];
             int indexMax = 0;
+            int min_element = array[0];
+            int min_index = 0;
+
+            //for(int i=1; i<array.Length; i++)
+            //{
+            //    if(array[i] < min_element)
+            //    {
+            //        min_element = array[i];
+            //        min_index = i;
+            //    }
+            //}
 
             for(int i = 1; i < array.Length; i++)
             {
                 if (array[i] > max) {
                     max = array[i]; indexMax = i;
                 }
+
+                if (array[i] < min_element)
+                {
+                    min_element = array[i];
+                    min_index = i;
+                }
             }
 
             Console.WriteLine($"max = {max}\t indexMax = {indexMax}");
+            Console.WriteLine("Minimal element:" + min_element);
+            Console.WriteLine("Minimal index:" + min_index);
         }
     }
 }
